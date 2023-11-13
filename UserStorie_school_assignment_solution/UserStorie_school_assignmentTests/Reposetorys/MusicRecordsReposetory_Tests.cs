2b4d956689f3892668c599a9cbf039ebd4576332
philipv99 <philipv9913@gmail.com>
2023-11-13 10:20:17 +0100

    more

--- UserStorie_school_assignment_solution/UserStorie_school_assignmentTests/Reposetorys/MusicRecordsReposetory_Tests.cs
+++ UserStorie_school_assignment_solution/UserStorie_school_assignmentTests/Reposetorys/MusicRecordsReposetory_Tests.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserStorie_school_assignment.Reposetorys;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UserStorie_school_assignmentTests.Reposetorys
+namespace UserStorie_school_assignment.Reposetorys.Tests
 {
-    internal class MusicRecordsReposetory_Tests
+    [TestClass()]
+    public class MusicRecordsReposetory_Tests
     {
+        [TestMethod()]
+        public void MusicRecordsReposetory_Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void Get_Test()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
